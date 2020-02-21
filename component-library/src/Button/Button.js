a6b4abc8b4b31c8c60fa15825a3f2871a2a75737
Joshua Reed <joshualreed93@gmail.com>
2020-02-21 16:02:26 -0500

    buttons created

---
+++ component-library/src/Button/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.css'
+
+function Button(props) {
+    let classList = '';
+    let types = ['primary', 'danger', 'success', 'warning','primary-outline','danger-outline','success-outline','warning-outline','faded-primary','faded-danger','faded-success','faded-warning','cart']
+    if(types.includes(props.type)){
+        classList += `button-${props.type}`
+    }
+    return (
+    <button className={classList}>{props.label}</button>
+    );
+}
+
+export default Button;
---
+++ component-library/src/Button/Button.stories.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import cart from '../../img/cart_2.png'
+
+import Button from './Button';
+
+storiesOf('Button', module)
+.add('Primary', ()=> <Button 
+    label='primary'
+    type='primary'
+/>)
+.add('Danger', () => <Button 
+label="Danger"
+type="danger"
+ 
+/>)
+.add('Success', () => <Button 
+label="Success"
+type="success"
+
+ />)
+.add('Warning', () => <Button 
+label="Warning"
+type="warning"
+
+/>)
+.add('Hollow-primary', () => <Button 
+label="primary-outline"
+type="primary-outline"
+
+/>)
+.add('Hollow-danger', () => <Button 
+label="danger-outline"
+type="danger-outline"
+
+/>)
+.add('Hollow-success', () => <Button 
+label="success-outline"
+type="success-outline"
+
+/>)
+.add('Hollow-warning', () => <Button 
+label="warning-outline"
+type="warning-outline"
+
+/>)
+.add('Faded-primary', ()=> <Button 
+label='faded-primary'
+type='faded-primary'
+/>)
+.add('Faded-danger', ()=> <Button 
+label='faded-danger'
+type='faded-danger'
+/>)
+.add('Faded-success', ()=> <Button 
+label='faded-success'
+type='faded-success'
+/>)
+.add('Faded-warning', ()=> <Button 
+label='faded-warning'
+type='faded-warning'
+/>)
+.add('Add-to-cart', ()=> <Button 
+icon='cart'
+label='Add-to-cart'
+type='add-cart'
+/>)
+.add('Cart', ()=> <Button 
+icon='cart'
+label=''
+type='cart'
+/>)
